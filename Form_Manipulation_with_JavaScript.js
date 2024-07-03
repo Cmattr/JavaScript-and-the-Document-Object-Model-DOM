afbd2114622a66a2b4a127614f8e8bb554541a20
Cmattr <cmattr@outlook.com>
2024-07-03 00:49:11 -0400

    Final

--- Form_Manipulation_with_JavaScript.js
+++ Form_Manipulation_with_JavaScript.js
@@ -2,19 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('userForm');
 
     form.addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevent the default form submission
+        event.preventDefault(); 
 
-        // Create an object to store form data
         const formData = {
             username: document.getElementById('username').value,
             email: document.getElementById('email').value,
             message: document.getElementById('message').value
         };
 
-        // Log the form data object to the console (you can also use it as needed)
+ 
         console.log(formData);
 
-        // Optionally, reset the form after submission
         form.reset();
     });
 });
